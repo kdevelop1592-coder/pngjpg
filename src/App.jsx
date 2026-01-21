b75b3f4be8214ac7bf47cdcfa995f46c79ea9a98
kalnal0928 <kalnal0928@gmail.com>
2026-01-21 13:05:43 +0900

    버그 픽스

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import PixelViewer from './components/PixelViewer';
 import DataInspector from './components/DataInspector';
